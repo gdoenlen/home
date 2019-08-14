fbc96cbec5b70ff9fe668d95ad129a005c0e2732
George Doenlen <gdoenlen@gmail.com>
2019-08-14 14:00:44 -0400

    call ps setup script when running wsl

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # New computer set up script
+set -e
 
 apt update
 apt upgrade -y
@@ -13,6 +14,12 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/m
 # as that is where this script lives
 ln -s ~/code/dotfiles/.zshrc ~/.zshrc
 ln -s ~/code/dotfiles/theme.zsh-theme ~/.oh-my-zsh/custom/themes/theme.zsh-theme
+
+# check if we're running WSL and setup our windows environment
+if [[ $(uname -r | cut -d '-' -f 3) == "Microsoft" ]]; then
+    echo WSL Detected running powershell setup script..
+    powershell.exe setup.ps1
+fi
 
 # finally change shell to zsh
 chsh -s $(command -v zsh)
