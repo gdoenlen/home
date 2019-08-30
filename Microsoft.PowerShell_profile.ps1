b4dd2140ab6c140f37b18f7adef59adf0d6fe801
George Doenlen <gdoenlen@gmail.com>
2019-08-30 12:47:57 -0400

    add aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -9,6 +9,14 @@ if (!(get-module -listavailable -name ZLocation)) {
 import-module posh-git
 import-module ZLocation
 
+set-alias -name from-json -value ConvertFrom-Json
+set-alias -name to-json -value ConvertTo-Json
+set-alias -name from-string -value ConvertFrom-String
+set-alias -name from-csv -value ConvertFrom-Csv
+set-alias -name to-csv -value ConvertTo-Csv
+set-alias -name to-xml -value ConvertTo-Xml
+set-alias -name grep -value Select-String
+
 # start fzf at the top
 $fzf = whereis fzf
 function fzf {
