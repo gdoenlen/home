c425add5f1a6c7f8859d1a78a927a210dc3e2208
George Doenlen <gdoenlen@gmail.com>
2021-09-18 10:19:22 -0400

    add vscode extensions

--- setup.ps1
+++ setup.ps1
@@ -1,34 +1,34 @@
 Write-Host 'starting setup script..'
 
-# platform wasn't added to the version table until 
-# pwsh 6 (the cross platform version), so if it's
-# null it means we're running pwsh 5 on windows
-if ('Win32NT', $null -contains $psversiontable.platform) {
-    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+Set-ExecutionPolicy RemoteSigned -scope CurrentUser
 
-    Write-Host 'installing scoop..'
-    Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')
-    scoop bucket add extras
-    scoop bucket add java
-    scoop bucket add nonportable
+Write-Host 'installing scoop..'
+Invoke-Expression (New-Object net.webclient).downloadstring('https://get.scoop.sh')
+scoop bucket add extras
+scoop bucket add java
+scoop bucket add nonportable
 
-    scoop update
+scoop update
 
-    Write-Host 'installing programs..'
-    scoop install vscode 7zip kdiff3 autohotkey `
-    jabba jmc git maven psutils `
+Write-Host 'installing programs..'
+scoop install vscode 7zip kdiff3 autohotkey `
+jmc git maven adoptopenjdk-hotspot`
 
-    Write-Host 'copying terminal settings..'
-    $packages = "$(Split-Path $env:APPDATA)/local/packages"
-    Get-ChildItem $packages `
+Write-Host 'installing editor extensions..'
+code --install-extension vscjava.vscode-java-pack
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension redhat.fabric8-analytics
+code --install-extension eamodio.gitlens
+code --install-extension SonarSource.sonarlint-vscode
+code --install-extension vscodevim.vim
+
+Write-Host 'copying terminal settings..'
+$packages = "$(Split-Path $env:APPDATA)/local/packages"
+Get-ChildItem $packages `
     | Select-Object -ExpandProperty Name `
     | Where-Object { $_ -match 'Microsoft.WindowsTerminal' } `
     | ForEach-Object { Copy-Item ./profiles.json "$packages/$_/localstate/profiles.json" }
-} else {
-    apt update
-    # todo
-}
-
+ 
 Write-Host 'installing ps modules..'
 Install-Module posh-git -scope currentuser -force
 Install-Module ZLocation -scope currentuser -force
